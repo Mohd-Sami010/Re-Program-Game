f85eff968361bb69798409edb6da526e3d2b38cb
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2025-12-21 16:06:38 +0530

    Balanced Eco a bit

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -12,11 +12,26 @@ public class ScoreManager :MonoBehaviour {
     }
     public int GetLevelReward()
     {
-        int levelScore = 6 + UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
-        int playTimeScore = minExpectedLevelPlayTime * 5 / GameManager.Instance.GetLevelPlayTime();
-        int numOfSnippetsScore = minNumOfSnippets * 5 / SnippetsManagerUI.Instance.GetNumberOfSnippetsUsed();
+        int levelScore = 2 + UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        int playTimeScore = minExpectedLevelPlayTime * 2 / GameManager.Instance.GetLevelPlayTime();
+        int numOfSnippetsScore = minNumOfSnippets * 2 / SnippetsManagerUI.Instance.GetNumberOfSnippetsUsed();
 
         return (levelScore + playTimeScore + numOfSnippetsScore);
+    }
+    public string GetLevelReport()
+    {
+        int diffrenceInTime = GameManager.Instance.GetLevelPlayTime() - minExpectedLevelPlayTime;
+        //string diffrenceInTimeStr =
+        //// Color a specific word using a named color tag
+        //myText.text = "I want to change the <color=\"blue\">color</color> of this word.";
+
+        //// Or use a hexadecimal color value
+        //// myText.text = "I want this part to be <color=#FF0000>red</color>.";
+        string levelReport = 
+            $"Level: {UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex}\n" +
+            $"Time Taken: {GameManager.Instance.GetLevelPlayTimeString()}\n"+
+            $"Code blocks used: {SnippetsManagerUI.Instance.GetNumberOfSnippetsUsed()}";
+        return levelReport;
     }
 
 }
